0f32cab49a24d00c3092a529b8d95fd9fd168e3d
LaTisha Habersham <72284231+llhabers@users.noreply.github.com>
2020-11-27 19:34:55 -0500

    PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,30 +9,27 @@ BudgetData = os.path.join("..", "Resources", "budget_data.csv")
 # Read CSV to ensure I was able to pull the data.
 with open(BudgetData, "r") as cvsfile:
 
-    #CSV reader specifies delimiter and variable that holds contents
+    # CSV reader specifies delimiter and variable that holds contents
     csvreader =csv.reader(cvsfile, delimiter=",")
-
-    print(csvreader)
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
 # Must make an empty set to hold the total months and net total of profits/loses.
 
     num_months = []
     profit_loses = []
    
-    # Read each row of data after the header
-    for row in csvreader:
-        num_months.append(row[0])
-        profit_loses.append(float(row[1]))
+    # Read each column of data after the header
+    for column in csvreader:
+        num_months.append(column[0])
+        profit_loses.append(int(column[1]))
 
 # We have to find the changes in "Profit/Losses" over the entire period, then find the average of those changes in the dataset.
     change_in_profit = []
 
     for i in range(1, len(profit_loses)):
-        change_in_profit.append(float(profit_loses[i]) - float(profit_loses[i-1]))
+        change_in_profit.append((int(profit_loses[i]) - int(profit_loses[i-1])))
 
 # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
     averageprofit = (sum(change_in_profit) / len(change_in_profit))
@@ -53,7 +50,7 @@ greatest_increase_month = change_in_profit.index(max(change_in_profit)) + 1
 
 greatest_decrease_month = change_in_profit.index(min(change_in_profit)) + 1 
 
-# check my answers
+# check my answers. If correct, then print we must output the results to a text file.
 
 print("Financial Analysis")
 print("---------------------------------------------------------------")
@@ -64,3 +61,17 @@ print(greatest_decrease)
 print(greatest_increase)
 print("Greates Increase in Profits:" + str(greatest_increase_month) + " " "$" + str(greatest_increase))
 print("Greates Decrease in Profits:" + str(greatest_decrease_month) + " " "$" + str(greatest_decrease))
+
+# We will output the Financial Analysiss on a text file. Don't forget to add \n to ensure you move to the next line.
+# output_file = Path("python-challenge", "PyBank", "analysis", "Financial_Analysis_Summary.txt")
+
+# with open(output_file,"w") as file:
+    # file = open("output.text", "w")
+    # file.write("Financial Analysis" + "\n")
+    # file.write("-------------------------------------------------------------" "\n")
+    # file.write("Total Months:" " " + str(total_months) + "\n")
+    # file.write("Total:" " " + "$" + str(sum(profit_loses)) + "\n")
+    # file.write("Average Change:" " " + "$" + str(averageprofit) + "\n")
+    # file.write("Greatest Increase in Profits:" + str(greatest_increase_month) + " " + "$" + str(greatest_increase) + "\n")
+    # file.write("Greatest Decrease in Profits:" + str(greatest_decrease_month) + " " + "$" + str(greatest_decrease) + "\n")
+    # file.close()
